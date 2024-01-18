3a889e370c3a61dae6d29935d78d964971bf3350
murkl <murkl@users.noreply.github.com>
2024-01-18 15:00:45 +0100

    1.1.5

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@ set -Eeuo pipefail
 # ----------------------------------------------------------------------------------------------------
 
 # Version
-VERSION='1.1.4'
+VERSION='1.1.5'
 
 # Title
 TITLE="Arch OS Installer ${VERSION}"
