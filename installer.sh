c7547c8e593ddfad344daea8b13f37dcb79bfe8b
murkl <murkl@users.noreply.github.com>
2024-07-30 16:46:04 +0200

    fuser

--- installer.sh
+++ installer.sh
@@ -763,13 +763,10 @@ exec_init_installation() {
         timedatectl set-ntp true # Set time
         # Make sure everything is unmounted before start install
         swapoff -a || true
-        fuser -m /mnt || true
+        fuser -km /mnt || true
         umount -f -A -R /mnt || true
-        fuser -m "$ARCH_OS_ROOT_PARTITION" || true
         umount -f -R "$ARCH_OS_ROOT_PARTITION" || true
-        fuser -m "$ARCH_OS_BOOT_PARTITION" || true
         umount -f -R "$ARCH_OS_BOOT_PARTITION" || true
-        fuser -m "$ARCH_OS_DISK" || true
         umount -f -R "$ARCH_OS_DISK" || true
         cryptsetup close cryptroot || true
         vgchange -an || true
