e9e7bdd08f8556a3e7a9ca97efa9ecf9b58ff487
murkl <murkl@users.noreply.github.com>
2024-09-20 00:52:43 +0200

    add utils

--- installer.sh
+++ installer.sh
@@ -914,7 +914,7 @@ exec_install_desktop() {
                 packages+=(samba gvfs gvfs-mtp gvfs-smb gvfs-nfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-google gvfs-dnssd gvfs-wsdd)
 
                 # Utils (https://wiki.archlinux.org/title/File_systems)
-                packages+=(fwupd bash-completion dhcp net-tools inetutils nfs-utils e2fsprogs f2fs-tools udftools dosfstools ntfs-3g exfat-utils btrfs-progs xfsprogs p7zip zip unzip unrar tar)
+                packages+=(archlinux-contrib pacutils fwupd bash-completion dhcp net-tools inetutils nfs-utils e2fsprogs f2fs-tools udftools dosfstools ntfs-3g exfat-utils btrfs-progs xfsprogs p7zip zip unzip unrar tar)
 
                 # Runtimes & Helper
                 packages+=(jq zenity gum)
