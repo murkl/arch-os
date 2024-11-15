3e707a76407edec089c0a1d17be72202a974b97e
murkl <murkl@users.noreply.github.com>
2024-11-15 22:07:01 +0100

    order

--- installer.sh
+++ installer.sh
@@ -148,14 +148,14 @@ main() {
     exec_prepare_disk
     exec_pacstrap_core
     exec_enable_multilib
-    exec_install_bootsplash
     exec_install_aur_helper
-    exec_install_housekeeping
-    exec_install_shell_enhancement
+    exec_install_bootsplash
     exec_install_desktop
     exec_install_graphics_driver
-    exec_install_archos_manager
     exec_install_vm_support
+    exec_install_shell_enhancement
+    exec_install_housekeeping
+    exec_install_archos_manager
     exec_cleanup_installation
 
     # Calc installation duration
