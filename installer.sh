f29bd68a1bc7accbdbf43708d1612b971104ae26
murkl <murkl@users.noreply.github.com>
2024-11-18 19:53:09 +0100

    pacman install

--- installer.sh
+++ installer.sh
@@ -1758,10 +1758,9 @@ chroot_pacman_install() {
         # Print log if greather than first try
         [ "$i" -gt 1 ] && log_warn "${i}. Retry Pacman installation..."
         # Try installing packages
+        # if ! arch-chroot /mnt bash -c "yes | LC_ALL=en_US.UTF-8 pacman -S --needed --disable-download-timeout ${packages[*]}"; then
         if ! arch-chroot /mnt pacman -S --noconfirm --needed --disable-download-timeout "${packages[@]}"; then
-            if ! arch-chroot /mnt bash -c "yes | LC_ALL=en_US.UTF-8 pacman -S --needed --disable-download-timeout ${packages[*]}"; then
-                sleep 10 && continue # Wait 10 seconds & try again
-            fi
+            sleep 10 && continue # Wait 10 seconds & try again
         else
             pacman_failed="false" && break # Success: break loop
         fi
