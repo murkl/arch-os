05735a919864c91012b833c14d722b53f58bd80b
murkl <murkl@users.noreply.github.com>
2025-01-22 11:41:06 +0100

    add: fd

--- installer.sh
+++ installer.sh
@@ -1529,7 +1529,7 @@ exec_install_shell_enhancement() {
             [ "$DEBUG" = "true" ] && sleep 1 && process_return 0 # If debug mode then return
 
             # Install packages
-            local packages=(git starship eza bat zoxide fastfetch mc btop nano neovim python-pynvim man-db bash-completion nano-syntax-highlighting ttf-firacode-nerd ttf-nerd-fonts-symbols)
+            local packages=(git starship eza bat zoxide fd fzf fastfetch mc btop nano neovim python-pynvim man-db bash-completion nano-syntax-highlighting ttf-firacode-nerd ttf-nerd-fonts-symbols)
             chroot_pacman_install "${packages[@]}"
 
             # Create fastfetch config dirs
