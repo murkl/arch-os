2acd54219d6c5dc30c3f599e3fd1b408e83d660a
murkl <murkl@users.noreply.github.com>
2025-03-25 21:49:14 +0100

    1.8.4

--- installer.sh
+++ installer.sh
@@ -21,7 +21,7 @@ set -E          # ERR trap inherited by shell functions (errtrace)
 : "${FORCE:=false}" # FORCE=true ./installer.sh
 
 # SCRIPT
-VERSION='1.8.3'
+VERSION='1.8.4'
 
 # GUM
 GUM_VERSION="0.13.0"
