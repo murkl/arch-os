5384895611354454adcb3ba4f87546ead4989ee6
Moritz Schaal <moritz.schaal@svlfg.de>
2025-07-28 07:42:43 +0200

    1.9.1

--- installer.sh
+++ installer.sh
@@ -21,7 +21,7 @@ set -E          # ERR trap inherited by shell functions (errtrace)
 : "${GUM:=/usr/local/bin/gum}" # GUM=/usr/bin/gum ./installer.sh
 
 # SCRIPT
-VERSION='1.9.0'
+VERSION='1.9.1'
 
 # VERSION
 [ "$*" = "--version" ] && echo "$VERSION" && exit 0
