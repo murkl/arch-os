354b22870855ecf631aaadde88cb730ab7787315
murkl <murkl@users.noreply.github.com>
2026-07-12 17:59:20 +0200

    version

--- installer.sh
+++ installer.sh
@@ -21,7 +21,7 @@ set -E          # ERR trap inherited by shell functions (errtrace)
 : "${GUM:=/usr/local/bin/gum}" # GUM=/usr/bin/gum ./installer.sh
 
 # SCRIPT
-VERSION='1.9.6'
+VERSION='1.9.7'
 
 # VERSION
 [ "$*" = "--version" ] && echo "$VERSION" && exit 0
